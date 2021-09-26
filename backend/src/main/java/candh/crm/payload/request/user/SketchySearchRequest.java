ffa088d179c70c21d1753f61032dce303ee006c2
Zhen <zzhcai.0xac372b70@gmail.com>
2021-09-26 20:06:38 +1000

    Payload fields @NotNull

--- backend/src/main/java/candh/crm/payload/request/user/SketchySearchRequest.java
+++ backend/src/main/java/candh/crm/payload/request/user/SketchySearchRequest.java
@@ -3,9 +3,12 @@ package candh.crm.payload.request.user;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class SketchySearchRequest
 {
+    @NotNull
     private String searchKey;
 }
