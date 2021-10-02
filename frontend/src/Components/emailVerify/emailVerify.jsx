f63a67b10621484eb2c1bf1683ab610f6b244606
Bingzhe Jin <jbzjbz@126.com>
2021-10-02 21:21:47 +1000

    Animation

--- frontend/src/Components/emailVerify/emailVerify.jsx
+++ frontend/src/Components/emailVerify/emailVerify.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./emailVerify.css";
 import fireworks from "react-fireworks";
 import TweenOne from "rc-tween-one";
+import "animate.css";
 
 class EmailVerify extends Component {
   constructor(props) {
@@ -14,10 +15,14 @@ class EmailVerify extends Component {
       background: "div-verify-background",
       waiting: false,
       paused: true,
+      click: true,
+      welcome: false,
     };
 
     this.startFlash = this.startFlash.bind(this);
     this.endFlash = this.endFlash.bind(this);
+    this.verifiedAnimation = this.verifiedAnimation.bind(this);
+    this.verify = this.verify.bind(this);
   }
 
   componentDidMount() {
@@ -56,26 +61,44 @@ class EmailVerify extends Component {
   }
 
   verifiedAnimation() {
+    this.setState({ paused: false, opacity: 1 });
     fireworks.init("root", {});
     fireworks.start();
     setTimeout(() => {
+      this.setState({ welcome: true });
       fireworks.stop();
     }, 5000);
   }
 
+  verify() {
+    this.setState({ click: false });
+    this.startFlash();
+    setTimeout(() => {
+      this.endFlash();
+      this.setState({ waiting: true });
+      this.verifiedAnimation();
+    }, 2000);
+  }
+
   render() {
-    const { waiting, background, opacity, paused } = this.state;
+    const { waiting, background, opacity, paused, click, welcome } = this.state;
     return (
       <div className={background}>
         {!waiting && (
           <p className="p-click-instruction">Click Here to Verify ➜</p>
         )}
         <TweenOne
+          animation={{ scale: 100, duration: 5000 }}
           paused={paused}
           style={{ opacity: opacity }}
           className="div-animate-circle"
         />
-        <button className="btn-verify" onClick={this.verifiedAnimation} />
+        {click && <button className="btn-verify" onClick={this.verify} />}
+        {welcome && (
+          <p className="p-welcome animate__animated animate__bounceIn">
+            Welcome
+          </p>
+        )}
       </div>
     );
   }
